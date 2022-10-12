321530ae08144c657d4cf54b59fd65917c19dd5d
wmc <wmc@phix.cn>
2022-10-12 11:42:55 +0800

    remove login

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ const i18n = new VueI18n({
 router.beforeEach((to, from, next) => {
     document.title = `${to.meta.title} | vue-manage-system`;
     const role = localStorage.getItem('ms_username');
-    if (!role && to.path !== '/login') {
+    if (0 && !role && to.path !== '/login') {
         next('/login');
     } else if (to.meta.permission) {
         // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
